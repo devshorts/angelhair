887901f57753dbd544bc5ea3ee0eac60e90cfc13
Jake Swenson <jswenson@godaddy.com>
2015-11-12 13:12:11 -0800

    adding message table columns

--- core/src/main/java/com/godaddy/domains/cassandraqueue/dataAccess/Tables.java
+++ core/src/main/java/com/godaddy/domains/cassandraqueue/dataAccess/Tables.java
@@ -1,10 +1,23 @@
 package com.godaddy.domains.cassandraqueue.dataAccess;
 
-public class Tables {
+public final class Tables {
     public static class Pointer {
         public static final String TABLE_NAME = "pointer";
         public static final String QUEUENAME = "queuename";
         public static final String POINTER_TYPE = "pointer_type";
         public static final String VALUE = "value";
+    }
+
+    public static class Message {
+
+        public static final String TABLE_NAME = "message";
+        public static final String QUEUENAME = "queuename";
+        public static final String BUCKET_NUM = "bucket_num";
+        public static final String MONOTON = "monoton";
+        public static final String MESSAGE = "blob";
+        public static final String VERSION = "blob";
+        public static final String ACKED = "blob";
+        public static final String NEXT_VISIBLE_ON = "next_visible_on";
+        public static final String CREATED_DATE = "created_date";
     }
 }
