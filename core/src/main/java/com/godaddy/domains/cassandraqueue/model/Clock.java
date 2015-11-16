ff210adb4a8d83b2a46ebbaf33a94a8e2fb7d6b7
Jake Swenson <jswenson@godaddy.com>
2015-11-16 09:12:50 -0800

    improve clock

--- core/src/main/java/com/godaddy/domains/cassandraqueue/model/Clock.java
+++ core/src/main/java/com/godaddy/domains/cassandraqueue/model/Clock.java
@@ -2,9 +2,12 @@ package com.godaddy.domains.cassandraqueue.model;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.Duration;
 import org.joda.time.Instant;
 
 public interface Clock {
     Instant now();
+
+    void sleepFor(Duration duration) throws InterruptedException;
 }
 
--- core/src/main/java/com/godaddy/domains/cassandraqueue/model/JodaClock.java
+++ core/src/main/java/com/godaddy/domains/cassandraqueue/model/JodaClock.java
@@ -1,5 +1,6 @@
 package com.godaddy.domains.cassandraqueue.model;
 
+import org.joda.time.Duration;
 import org.joda.time.Instant;
 
 public final class JodaClock implements Clock {
@@ -7,5 +8,10 @@ public final class JodaClock implements Clock {
     @Override
     public Instant now() {
         return Instant.now();
+    }
+
+    @Override
+    public void sleepFor(final Duration duration) throws InterruptedException {
+        Thread.sleep(duration.getStandardSeconds() * 1000L);
     }
 }
